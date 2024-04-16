fcc3309f7b5e87937b35bbfe15a38467e7c14462
Raul <raul.gonzalez1@alumnos.uneatlantico.es>
2024-04-16 18:02:23 +0200

    Conect back with postgre db

--- app.js
+++ app.js
@@ -4,72 +4,76 @@ const bcrypt = require("bcrypt");
 var cors = require("cors");
 const jwt = require("jsonwebtoken");
 
+//For conection with db
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "tfg",
+  password: "admin",
+  port: 5432,
+});
+
+
 const app = express();
 const PORT = 3080;
-
-const usersFilePath = "./users.json";
 
 // Middleware to parse JSON bodies
 app.use(cors())
 app.use(express.json());
 
 // Register endpoint
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const { email, password } = req.body;
 
-  // Read existing users from the file
-  const usersData = JSON.parse(fs.readFileSync(usersFilePath, "utf8"));
+  //Check if entry exists in db
+  const result = (await pool.query("SELECT * FROM users WHERE email = $1", [email]));
 
-  // Check if email is already taken
-  const userExists = usersData.find((user) => user.email === email);
-  if (userExists) {
-    return res.status(400).json({ message: "Email already registered" });
-  }
+  if (result.rows.length > 0) return res.status(400).json({ message: "Email already registered" });
 
   // Hash the password
   bcrypt.hash(password, 10, (err, hash) => {
     if (err) {
       return res.status(500).json({ message: "Internal Server Error" });
     }
-
     // Save new user data
-    const newUser = { email, password: hash };
-    usersData.push(newUser);
-    fs.writeFileSync(usersFilePath, JSON.stringify(usersData, null, 2));
-
-    res.status(200).json({ message: "User registered successfully" });
+    pool.query('INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *', [email, hash], (error, result) => {
+      if (error) {
+        console.error('Error executing query', error);
+        return;
+      }
+      res.status(200).json({ message: "User registered successfully" });
+    });
   });
 });
 
 // Login endpoint
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   console.log("Login Attempt: ", email,password)
 
-  // Read existing users from the file
-  const usersData = JSON.parse(fs.readFileSync(usersFilePath, "utf8"));
-
   // Find user by email
-  const user = usersData.find((user) => user.email === email);
-  if (!user) {
-    return res.status(404).json({ message: "User not found" });
+  try {
+    const result = (await pool.query("SELECT * FROM users WHERE email = $1", [email])).rows[0];
+    // Compare passwords
+    bcrypt.compare(password, result.password, (err, result) => {
+      if (err) {
+        return res.status(500).json({ message: "Internal Server Error" });
+      }
+      if (!result) {
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+      // Generate JWT token
+      const token = jwt.sign({ email: email }, "secret", { expiresIn: "1h" });
+      res.status(200).json({ message: "Login successful", token });
+    })
+
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
   }
 
-  // Compare passwords
-  bcrypt.compare(password, user.password, (err, result) => {
-    if (err) {
-      return res.status(500).json({ message: "Internal Server Error" });
-    }
-
-    if (!result) {
-      return res.status(401).json({ message: "Invalid credentials" });
-    }
-
-    // Generate JWT token
-    const token = jwt.sign({ email: user.email }, "secret", { expiresIn: "1h" });
-    res.status(200).json({ message: "Login successful", token });
-  });
 });
 
 app.listen(PORT, () => {
