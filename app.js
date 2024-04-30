cbf34609b1558f82825358ae99f426df71903159
Raul <raul.gonzalez1@alumnos.uneatlantico.es>
2024-04-30 14:00:30 +0200

    Changed db conection to mongo

--- app.js
+++ app.js
@@ -2,19 +2,8 @@ const express = require("express");
 const fs = require("fs");
 const bcrypt = require("bcrypt");
 var cors = require("cors");
+const collection = require("./dbConfig");
 const jwt = require("jsonwebtoken");
-
-//For conection with db
-const { Pool } = require("pg");
-
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "tfg",
-  password: "admin",
-  port: 5432,
-});
-
 
 const app = express();
 const PORT = 3080;
@@ -25,55 +14,46 @@ app.use(express.json());
 
 // Register endpoint
 app.post("/register", async (req, res) => {
-  const { email, password } = req.body;
+  const data = {
+    email: req.body.email,
+    password: req.body.password
+}
 
   //Check if entry exists in db
-  const result = (await pool.query("SELECT * FROM users WHERE email = $1", [email]));
+  const existingEmail = await collection.findOne({ email: data.email });
 
-  if (result.rows.length > 0) return res.status(400).json({ message: "Email already registered" });
+  if (existingEmail) return res.status(400).json({ message: "Email already registered" });
 
   // Hash the password
-  bcrypt.hash(password, 10, (err, hash) => {
-    if (err) {
-      return res.status(500).json({ message: "Internal Server Error" });
-    }
-    // Save new user data
-    pool.query('INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *', [email, hash], (error, result) => {
-      if (error) {
-        console.error('Error executing query', error);
-        return;
-      }
-      res.status(200).json({ message: "User registered successfully" });
-    });
-  });
+  const hashedPassword = await bcrypt.hash(data.password, 10);
+  data.password = hashedPassword; // Replace the original password with the hashed one
+  const userdata = await collection.insertMany(data);
+  res.status(200).json({ message: "User registered successfully" });
 });
 
 // Login endpoint
 app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
+  const data = {
+    email: req.body.email,
+    password: req.body.password
+}
 
-  console.log("Login Attempt: ", email,password)
+  // Check if user exists in the database
+  const user = await collection.findOne({ email: data.email });
 
-  // Find user by email
-  try {
-    const result = (await pool.query("SELECT * FROM users WHERE email = $1", [email])).rows[0];
-    // Compare passwords
-    bcrypt.compare(password, result.password, (err, result) => {
-      if (err) {
-        return res.status(500).json({ message: "Internal Server Error" });
-      }
-      if (!result) {
-        return res.status(401).json({ message: "Invalid credentials" });
-      }
-      // Generate JWT token
-      const token = jwt.sign({ email: email }, "secret", { expiresIn: "1h" });
-      res.status(200).json({ message: "Login successful", token });
-    })
-
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
+  if (!user) {
+    return res.status(401).json({ message: "Invalid email or password" });
   }
 
+  // Compare the provided password with the hashed password stored in the database
+  const passwordMatch = await bcrypt.compare(data.password, user.password);
+
+  if (!passwordMatch) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+  // If authentication successful, return success message or token
+  res.status(200).json({ message: "Login successful" });
 });
 
 app.listen(PORT, () => {
