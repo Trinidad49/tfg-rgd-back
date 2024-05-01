831087b520d4adff4b5261939a48e3b14cfa3a15
Raul <raul.gonzalez1@alumnos.uneatlantico.es>
2024-05-01 12:52:03 +0200

    Adding survey options get/post

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const bcrypt = require("bcrypt");
 var cors = require("cors");
-const collection = require("./dbConfig");
+const {usersCollection,surveysCollection} = require("./dbConfig");
 const jwt = require("jsonwebtoken");
 
 const app = express();
@@ -20,14 +20,14 @@ app.post("/register", async (req, res) => {
 }
 
   //Check if entry exists in db
-  const existingEmail = await collection.findOne({ email: data.email });
+  const existingEmail = await usersCollection.findOne({ email: data.email });
 
   if (existingEmail) return res.status(400).json({ message: "Email already registered" });
 
   // Hash the password
   const hashedPassword = await bcrypt.hash(data.password, 10);
   data.password = hashedPassword; // Replace the original password with the hashed one
-  const userdata = await collection.insertMany(data);
+  const userdata = await usersCollection.insertMany(data);
   res.status(200).json({ message: "User registered successfully" });
 });
 
@@ -39,7 +39,7 @@ app.post("/login", async (req, res) => {
 }
 
   // Check if user exists in the database
-  const user = await collection.findOne({ email: data.email });
+  const user = await usersCollection.findOne({ email: data.email });
 
   if (!user) {
     return res.status(401).json({ message: "Invalid email or password" });
@@ -53,7 +53,48 @@ app.post("/login", async (req, res) => {
   }
 
   // If authentication successful, return success message or token
-  res.status(200).json({ message: "Login successful" });
+  res.status(200).json({ message: "Login successful", id:user._id });
+});
+
+app.get('/surveys', async (req, res) => {
+  console.log("Get survey")
+  try {
+    const userID = req.headers.userid; // Assuming user ID is available in request object
+    const surveys = await surveysCollection.find({ userID });
+    res.json(surveys);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// POST a new survey for the user
+app.post('/surveys', async (req, res) => {
+  try { // Assuming user ID is available in request object
+
+    const { userID,title, questions } = req.body;
+    console.log(title,userID)
+    console.log(req.body)
+
+    // Create a new survey document
+    const newSurvey = {
+      userID,
+      title,
+      question: [questions],
+      createdAt: new Date()
+    };
+    
+    console.log(newSurvey)
+
+
+    // Insert the new survey into the collection
+    await surveysCollection.insertMany(newSurvey);
+
+    res.status(201).json({ message: 'Survey created successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
 app.listen(PORT, () => {
--- dbConfig.js
+++ dbConfig.js
@@ -1,27 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const connect = mongoose.connect("mongodb://0.0.0.0:27017/TFG");
 
 // Check database connected or not
-connect.then(() => {
+connect
+  .then(() => {
     console.log("Database Connected Successfully");
-})
-.catch(() => {
+  })
+  .catch(() => {
     console.log("Database cannot be Connected");
-})
+  });
 
 // Create Schema
 const Loginschema = new mongoose.Schema({
-    email: {
-        type:String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const Surveyschema = new mongoose.Schema({
+  userID: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  questions: {
+    type: [String],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 // collection part
-const collection = new mongoose.model("user", Loginschema);
+const usersCollection = new mongoose.model("users", Loginschema);
+const surveysCollection = new mongoose.model("surveys", Surveyschema);
 
-module.exports = collection;
+module.exports = { usersCollection, surveysCollection };
